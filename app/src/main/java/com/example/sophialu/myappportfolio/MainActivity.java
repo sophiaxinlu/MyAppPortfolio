26ef2925f53764d73c4a38ed70843615941704ab
sophia.lu <sophia.lu@belkin.com>
2015-08-04 14:35:34 -0700

    Added project files

---
+++ app/src/main/java/com/example/sophialu/myappportfolio/MainActivity.java
@@ -0,0 +1,44 @@
+package com.example.sophialu.myappportfolio;
+
+import android.content.Context;
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+
+public class MainActivity extends ActionBarActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    final String intro = "This button will launch my ";
+
+    public void showToast(View v) {
+        Context context = getApplicationContext();
+
+        Button b = (Button)v;
+        String appName = b.getText().toString().toLowerCase();
+
+        CharSequence text = intro + appName + " app";
+        int duration = Toast.LENGTH_SHORT;
+
+        Toast toast = Toast.makeText(context, text, duration);
+        toast.show();
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+}
